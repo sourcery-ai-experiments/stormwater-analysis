13544ca594b61bf71083bdc2e92b83c1bd943b21
BuczynskiRafal <buczynski.rafal2@gmail.com>
2023-03-11 11:50:28 +0100

    Calculations with pipes

--- pipes/round.py
+++ pipes/round.py
@@ -1,10 +1,39 @@
 import math
 import logging
+
+import matplotlib.pyplot
 import matplotlib.pyplot as plt
 import pandas as pd
-from numpy import sin, cos, pi, linspace
+from numpy import sin, cos, pi, linspace, finfo
 
 logger = logging.getLogger(__name__)
+
+
+def check_dimensions(filling: float, diameter: float) -> bool:
+    """
+    Check if the given filling and diameter values are valid.
+
+    Args:
+        filling (float, int): the height of the filling in the pipe, in meters.
+        diameter (float, int): the diameter of the pipe, in meters.
+
+    Returns:
+        bool: True if the values are valid, False otherwise.
+
+    Raises:
+        TypeError: If either the filling or the diameter is not an int or float.
+        ValueError: If either the filling or the diameter is not between 0.2 and 2.0 meters.
+
+    """
+    if not isinstance(filling, (int, float)):
+        raise TypeError("Filling must be an int or float")
+    if not isinstance(diameter, (int, float)):
+        raise TypeError("Diameter must be an int or float")
+    if filling > diameter:
+        raise ValueError("Filling must be less than or equal to the diameter")
+    if not (0 <= filling <= 2.0 and 0.2 <= diameter <= 2.0):
+        raise ValueError("Value out of bounds. Filling must be between 0.2 and 2.0 meters, and diameter must be between 0.2 and 2.0 meters")
+    return True
 
 
 def max_filling(diameter: float) -> float:
@@ -70,17 +99,18 @@ def calc_f(filling: float, diameter: float) -> float:
     Return:
         area (int, float): cross-sectional area of the wetted part of the pipe [m2]
     """
-    radius = diameter / 2
-    chord = math.sqrt((radius**2 - ((filling - radius) ** 2))) * 2
-    alpha = math.acos((radius**2 + radius**2 - chord**2) / (2 * radius**2))
-    if filling > radius:
-        return pi * radius**2 - (1 / 2 * (alpha - math.sin(alpha)) * radius**2)
-    elif filling == radius:
-        return pi * radius**2 / 2
-    elif filling == diameter:
-        return pi * radius**2
-    else:
-        return 1 / 2 * (alpha - math.sin(alpha)) * radius**2
+    if check_dimensions(filling, diameter):
+        radius = diameter / 2
+        chord = math.sqrt((radius**2 - ((filling - radius) ** 2))) * 2
+        alpha = math.acos((radius**2 + radius**2 - chord**2) / (2 * radius**2))
+        if filling > radius:
+            return pi * radius**2 - (1 / 2 * (alpha - math.sin(alpha)) * radius**2)
+        elif filling == radius:
+            return pi * radius**2 / 2
+        elif filling == diameter:
+            return pi * radius**2
+        else:
+            return 1 / 2 * (alpha - math.sin(alpha)) * radius**2
 
 
 def calc_u(filling: float, diameter: float) -> float:
@@ -96,14 +126,15 @@ def calc_u(filling: float, diameter: float) -> float:
     Return:
         circumference (int, float): circumference of a wetted part of pipe
     """
-    radius = diameter / 2
-    chord = math.sqrt((radius**2 - (filling - radius) ** 2)) * 2
-    alpha = math.degrees(
-        math.acos((radius**2 + radius**2 - chord**2) / (2 * radius**2))
-    )
-    if filling > radius:
-        return 2 * math.pi * radius - (alpha / 360 * 2 * math.pi * radius)
-    return alpha / 360 * 2 * math.pi * radius
+    if check_dimensions(filling, diameter):
+        radius = diameter / 2
+        chord = math.sqrt((radius**2 - (filling - radius) ** 2)) * 2
+        alpha = math.degrees(
+            math.acos((radius**2 + radius**2 - chord**2) / (2 * radius**2))
+        )
+        if filling > radius:
+            return 2 * math.pi * radius - (alpha / 360 * 2 * math.pi * radius)
+        return alpha / 360 * 2 * math.pi * radius
 
 
 def calc_rh(filling: float, diameter: float) -> float:
@@ -120,13 +151,31 @@ def calc_rh(filling: float, diameter: float) -> float:
     Return:
         Rh (int, float): hydraulic radius [m]
     """
-    try:
-        return calc_f(filling, diameter) / calc_u(filling, diameter)
-    except ZeroDivisionError:
-        return 0
+    if check_dimensions(filling, diameter):
+        try:
+            return calc_f(filling, diameter) / calc_u(filling, diameter)
+        except ZeroDivisionError:
+            return 0
 
 
-def min_slope(filling: float, diameter: float) -> float:
+def calc_velocity(filling: float, diameter: float, slope: float) -> float:
+    """
+    Calculate the speed of the sewage flow in the sewer.
+
+    Args:
+        filling (int, float): pipe filling height [m]
+        diameter (int, float): pipe diameter [m]
+        slope (int, float): slope of the sewer bottom [‰]
+
+    Return:
+        v (int, float): sewage flow velocity in the sewer [m/s]
+    """
+    if check_dimensions(filling, diameter):
+        slope = slope / 1000
+        return 1 / 0.013 * calc_rh(filling, diameter) ** (2 / 3) * (slope ** 0.5)
+
+
+def min_slope(filling: float, diameter: float, theta: float = 1.5, g: float = 9.81) -> float:
     """
     Get the minimal slope for sewer pipe.
     If the pipe  filling is greater than 0.3,
@@ -138,8 +187,112 @@ def min_slope(filling: float, diameter: float) -> float:
     Args:
         filling (int, float): pipe filling height [m]
         diameter (int, float): pipe diameter [m]
+        theta (float, optional): theta value. Defaults to 1.5, shear stress [Pa].
+        g (float, optional): specific gravity of liquid (water/wastewater) [N/m3].
 
     Return:
         slope (int, float): The minimum slope of the channel [‰]
     """
-    return 0.612 * (10**-3) * ((diameter / 4) / calc_rh(filling, diameter)) * (1 / diameter)
+    return 4 * (theta / g) * ((diameter / 4) / calc_rh(filling, diameter)) * (1 / diameter)
+
+
+def max_slope(diameter: float) -> float:
+    slope = min_slope(diameter, diameter)
+    v_max = max_velocity()
+    v_clc = calc_velocity(diameter, diameter, slope)
+    while v_clc < v_max:
+        slope += 0.01
+        v_clc = calc_velocity(diameter, diameter, slope)
+        print(v_clc)
+    return f"slope: {slope:.2f}, v_clc: {v_clc:.2f}, v_max: {v_max:.2f}"
+
+
+print(max_slope(0.5))
+
+def draw_pipe_section(filling: float, diameter: float, max_filling: float = None) -> matplotlib.pyplot:
+    """
+    Plot a pipe section with a given diameter and filling height.
+
+    Args:
+        filling (int, float): pipe filling height [m]
+        diameter (int, float): pipe diameter [m]
+        max_filling (int, flow): The maximum filling of the pipe. If the pipe is filled above this level,
+            the wetted part of the pipe will be drawn in red
+    """
+    if max_filling is None:
+        max_filling = diameter
+
+    radius = diameter / 2
+    # draw center point  - 0, 0
+    plt.plot(0, 0, color="black", marker="o")
+    plt.gca().annotate(
+        "O (0, 0)",
+        xy=(0 + radius / 10, 0 + radius / 10),
+        xycoords="data",
+        fontsize=12,
+    )
+    plt.xlim(-radius - 0.05, radius + 0.05)
+    plt.ylim(-radius, radius + 0.07)
+    plt.gca().set_aspect("equal")
+
+    # draw circle
+    angels = linspace(0 * pi, 2 * pi, 100)
+    xs = radius * cos(angels)
+    ys = radius * sin(angels)
+    # add circle
+    plt.plot(xs, ys, color="brown", label=f"Pipe: DN {diameter} [m]")
+
+    # draw diameter
+    plt.plot(radius, 0, marker="o", color="blue")
+    plt.plot(-radius, 0, marker="o", color="blue")
+    plt.plot([radius, -radius], [0, 0])
+    # annotation to diameter
+    plt.gca().annotate(
+        f"Diameter={diameter}", xy=(radius / 8, -radius / 5), xycoords="data", fontsize=12
+    )
+
+    # draw level of water
+    plt.plot(0, -radius, marker="o", color="purple")
+    plt.plot(0, filling - radius, marker="o", color="purple")
+    plt.plot(
+        [0, 0],
+        [-radius, filling - radius],
+        color="purple",
+        label=f"Pipe filling height: {filling} [m]",
+    )
+    plt.gca().annotate(
+        f"Water lvl={filling}",
+        xy=(radius / 2, filling - radius + 0.01),
+        xycoords="data",
+        fontsize=12,
+    )
+
+    # Draw arc as created by water level
+    chord = math.sqrt((radius**2 - ((filling - radius) ** 2))) * 2
+    alpha = math.acos((radius**2 + radius**2 - chord**2) / (2 * radius**2))
+
+    if filling > max_filling:
+        color = "red"
+    else:
+        color = "blue"
+    # Create arc
+    if filling <= radius:
+        diff = math.radians(180) - alpha
+        arc_angles = linspace(diff / 2, alpha + diff / 2, 20)
+    else:
+        diff = math.radians(180) - alpha
+        arc_angles = linspace(-diff / 2, alpha + diff + diff / 2, 100)
+    arc_xs = radius * cos(arc_angles)
+    arc_ys = radius * sin(arc_angles)
+    plt.plot(arc_xs, -arc_ys, color=color, lw=3)
+    plt.plot(
+        [-arc_xs[0], -arc_xs[-1]],
+        [-arc_ys[0], -arc_ys[-1]],
+        marker="o",
+        color=color,
+        lw=3,
+        label=f"Wetted part of pipe: {calc_f(filling, diameter):.2f} [m2]",
+    )
+    plt.grid(True)
+    plt.legend(loc="upper left")
+    plt.show()
--- pipes/test_valid_round/test_round.py
+++ pipes/test_valid_round/test_round.py
@@ -1,30 +1,442 @@
 import math
 import pytest
 import numpy as np
-from pipes.round import (max_filling, max_velocity, min_velocity)
+from pipes.round import (check_dimensions, max_filling, max_velocity, min_velocity,
+                         calc_f, calc_u, calc_rh, min_slope, max_slope)
+
+
+class TestCheckDimensions:
+
+    def test_check_dimensions_filling_and_diameter_equal(self):
+        """
+        Test check_dimensions function when filling and diameter are equal.
+        """
+        assert check_dimensions(0.5, 0.5) is True
+
+    def test_check_dimensions_filling_lower_bound(self):
+        """
+        Test check_dimensions function when filling is at lower bound.
+        """
+        assert check_dimensions(0.2, 0.2) is True
+
+    def test_check_dimensions_filling_upper_bound(self):
+        """
+        Test check_dimensions function when filling is at upper bound.
+        """
+        assert check_dimensions(2.0, 2.0) is True
+
+    def test_check_dimensions_diameter_lower_than_bound(self):
+        """
+        Test check_dimensions function when diameter is lower than the lower bound.
+        """
+        with pytest.raises(ValueError):
+            check_dimensions(0.1, 0.1)
+
+    def test_check_dimensions_diameter_upper_bound(self):
+        """
+        Checks if the diameter is less than or equal to the upper bound
+        """
+        assert check_dimensions(0.5, 2.0) is True
+
+    def test_check_dimensions_filling_and_diameter_out_of_range(self):
+        """
+        Check that the filling and diameter are within the range of 0.1 to 2.0
+        """
+        with pytest.raises(ValueError):
+            check_dimensions(0.1, 2.1)
+
+    def test_check_dimensions_filling_and_diameter_not_specified(self):
+        """
+        This function checks the dimensions of the pizza to make sure that the filling is less than the diameter
+        """
+        with pytest.raises(TypeError):
+            check_dimensions()  # type: ignore
+
+    def test_check_dimensions_filling_and_diameter_with_none(self):
+        """
+        Check that the filling and diameter are both positive numbers
+        """
+        with pytest.raises(TypeError):
+            check_dimensions(None, None)  # type: ignore
+
+    def test_check_dimensions_valid_values(self):
+        """
+        Check if the dimensions of a rectangle are valid
+        """
+        assert check_dimensions(0.5, 1.0) is True
+
+    def test_check_dimensions_filling_greater_than_diameter(self):
+        """
+        Check that the filling is less than the diameter
+        """
+        with pytest.raises(ValueError):
+            check_dimensions(1.0, 0.5)
+
+    def test_check_dimensions_filling_not_float_or_int(self):
+        """
+        Check that the dimensions of the rectangle are floats or integers
+        """
+        with pytest.raises(TypeError):
+            check_dimensions("0.5", 1.0)  # type: ignore
+
+    def test_check_dimensions_diameter_not_float_or_int(self):
+        """
+        Check that the diameter and height are both floats or ints
+        """
+        with pytest.raises(TypeError):
+            check_dimensions(0.5, "1.0")  # type: ignore
+
+    def test_check_dimensions_filling_out_of_range(self):
+        """
+        Check that the dimensions of the input are valid
+        """
+        with pytest.raises(ValueError):
+            check_dimensions(-1, 1.0)
+
+    def test_check_dimensions_diameter_out_of_range(self):
+        """
+        Check that the diameter and height of a cylinder are both positive and the diameter is no
+        greater than 2.1
+        """
+        with pytest.raises(ValueError):
+            check_dimensions(0.5, 2.1)
 
 
 class TestMaxFilling:
+    """Class contain tests the max_filling function."""
 
     def test_valid_input(self):
+        """
+        Tests that the function `max_filling` returns the correct value for three different inputs
+        """
         assert math.isclose(max_filling(0.2), 0.1654, rel_tol=1e-4)
         assert math.isclose(max_filling(1.0), 0.827, rel_tol=1e-4)
         assert math.isclose(max_filling(2.0), 1.654, rel_tol=1e-4)
 
     def test_max_filling_valid_values(self):
+        """
+        Check if max_filling takes a diameter and returns the maximum filling for that diameter
+        """
         for val, dia in zip([0.165, 0.248, 0.331, 0.414, 0.496, 0.579, 0.662, 0.744, 0.827], np.arange(0.2, 1, 0.1)):
             assert pytest.approx(val) == round(max_filling(dia), 3)
 
     def test_invalid_input(self):
+        """
+        Tests the max_filling function to make sure that it raises a TypeError if the input is not a number,
+        and a ValueError if the input is not an integer between 1 and 3
+        """
         with pytest.raises(TypeError):
-            max_filling("invalid")
+            max_filling("invalid")  # type: ignore
         with pytest.raises(ValueError):
             max_filling(0.1)
         with pytest.raises(ValueError):
             max_filling(3.0)
 
+
+class TestMaxVelocity:
+    """
+    Class contain tests the max velocity of runoff water in sewer pipe.
+    """
+
     def test_max_velocity(self):
+        """
+        Test if max velocity returns the maximum velocity of the robot
+        """
         assert max_velocity() == 3
 
     def test_min_velocity(self):
+        """
+        Test if max velocity returns the minimum velocity of the car.
+        """
         assert min_velocity() == 0.7
+
+
+class TestCalcF:
+    """
+    Class contain tests the calc_f function.
+    """
+
+    def test_calc_f_zero_filling(self):
+        """
+        Test if calc_f returns 0 when the second argument is 0
+        """
+        assert calc_f(0, 1) == 0
+        assert calc_f(0, 2) == 0
+        assert calc_f(0, 0.5) == 0
+        assert calc_f(0, 0.2) == 0
+
+    def test_calc_f_filling_equal_diameter(self):
+        """
+        Test if calc_f calculates the filling factor of a circle with a given diameter and a given hole diameter
+        """
+        assert calc_f(1, 1) == 0.7853981633974483
+        assert calc_f(2, 2) == 3.141592653589793
+        assert calc_f(0.5, 0.5) == 0.19634954084936207
+        assert calc_f(0.1, 0.2) == 0.015707963267948967
+
+    def test_calc_f_filing_equal_radius(self):
+        """
+        Test if calc_f calculates the angle of the arc of a circle with a given radius and chord length
+        """
+        assert calc_f(0.5, 1) == 0.39269908169872414
+        assert calc_f(1, 2) == 1.5707963267948966
+        assert calc_f(0.25, 0.5) == 0.09817477042468103
+        assert calc_f(0.1, 0.2) == 0.015707963267948967
+
+    def test_calc_f_filing_greater_than_radius(self):
+        """
+        Test if calc_f function raises a ValueError when the filing_radius is greater than the radius
+        """
+        with pytest.raises(ValueError):
+            calc_f(1.5, 1)
+            calc_f(2.5, 2)
+            calc_f(0.75, 0.5)
+            calc_f(0.4, 0.3)
+
+    def test_calc_f_raises_exception(self):
+        """
+        Test if calc_f() raises a TypeError exception if the arguments are not of type int
+        """
+        with pytest.raises(TypeError):
+            calc_f("1", 2)  # type: ignore
+        with pytest.raises(TypeError):
+            calc_f(1, "2")  # type: ignore
+        with pytest.raises(TypeError):
+            calc_f(1.5, "2.5")  # type: ignore
+
+    def test_calc_f_filing_greater_than_diameter(self):
+        """
+        Test if calc_f() raises a ValueError if the filing diameter is greater than the pipe diameter
+        """
+        with pytest.raises(ValueError):
+            calc_f(3, 1)
+        with pytest.raises(ValueError):
+            calc_f(4, 2)
+        with pytest.raises(ValueError):
+            calc_f(1.1, 1)
+
+    def test_calc_f_filling_less_than_zero(self):
+        """
+        Test if calc_f raises a `ValueError` when the filling is less than zero
+        """
+        with pytest.raises(ValueError):
+            calc_f(-1, 1)
+        with pytest.raises(ValueError):
+            calc_f(-0.5, 0.5)
+
+
+class TestCalcU:
+    """
+    Class contain tests the calc_u function.
+    """
+
+    def test_calc_u_with_valid_input(self):
+        """
+        Test if calc_u function with valid input
+        """
+        assert round(calc_u(1, 2), 3) == 3.142
+
+    def test_calc_u_with_zero_filling(self):
+        """
+        Test if calc_u work with zero filling
+        """
+        assert round(calc_u(0, 2), 3) == 0
+
+    def test_calc_u_with_filling_above_radius(self):
+        """
+        Test if calc_u raises a `ValueError` when the filling is greater than 1
+        """
+        with pytest.raises(ValueError):
+            calc_u(2.5, 2)
+
+    def test_calc_u_with_negative_filling(self):
+        """
+        Test if calc_u raises a `ValueError` when the filling is negative
+        """
+        with pytest.raises(ValueError):
+            calc_u(-1, 2)
+
+    def test_calc_u_with_negative_diameter(self):
+        """
+        Test if calc_u raises a `ValueError` when the diameter is negative
+        """
+        with pytest.raises(ValueError):
+            calc_u(1, -2)
+
+    def test_calc_u_with_invalid_dimensions(self):
+        """
+        Test if calc_u raises a ValueError if filling is greater than diameter.
+        """
+        with pytest.raises(ValueError):
+            calc_u(2, 1)
+
+    def test_calc_u_with_invalid_type_for_filling(self):
+        """
+        Test calc_u function with an invalid type for the `filling`
+        parameter
+        """
+        with pytest.raises(TypeError):
+            calc_u("1", 2)  # type: ignore
+
+    def test_calc_u_with_invalid_type_for_diameter(self):
+        """
+        Test if calc_u raises a `TypeError` when the diameter is not a number.
+        """
+        with pytest.raises(TypeError):
+            calc_u(1, [])  # type: ignore
+
+    def test_calc_u_with_no_dimensions_specified(self):
+        """
+        Test if calc_u raises a `TypeError` when no dimensions are specified.
+        """
+        with pytest.raises(TypeError):
+            calc_u()  # type: ignore
+
+    def test_calc_u_with_none_for_filling_and_diameter(self):
+        """
+        Test calc_u with invalid inputs.
+        """
+        with pytest.raises(TypeError):
+            calc_u(None, None)  # type: ignore
+
+
+class TestCalcRh:
+    """
+    Class contain tests the calc_rh function.
+    """
+
+    @pytest.mark.parametrize("filling, diameter, expected", [
+        (1.0, 0.5, ValueError),
+        (0.3, 0.3, 0.075),
+        (0.5, 1.0, 0.25),
+        (2.5, 1.0, ValueError),
+        (0.1, 0.1, ValueError),
+        (3, "two", TypeError),
+        (1, 2, 0.5),
+    ])
+    def test_calc_rh(self, filling, diameter, expected):
+        """
+        Test the calc_rh function with various input arguments.
+
+        Args:
+            filling (float or int): the filling height of the pipe, in meters.
+            diameter (float or int): the diameter of the pipe, in meters.
+            expected: the expected output of the function for the given input arguments.
+                      If it is a type, it indicates the expected exception type.
+
+        Raises:
+            AssertionError: if the actual output of the function does not match
+                             the expected output, or if it does not raise the
+                             expected exception.
+        """
+        if isinstance(expected, type) and issubclass(expected, Exception):
+            with pytest.raises(expected):
+                calc_rh(filling, diameter)
+        else:
+            assert calc_rh(filling, diameter) == expected
+
+    def test_calc_rh_valid_input(self):
+        """
+        Test valid inputs for the `calc_rh` function.
+
+        It tests whether the function returns the expected value for the given inputs.
+
+        """
+        assert calc_rh(0.5, 1.0) == 0.25
+        assert calc_rh(1.0, 1.0) == 0.25
+        assert calc_rh(0.2, 0.2) == 0.05
+        assert calc_rh(2.0, 2.0) == 0.5
+
+    def test_calc_rh_invalid_input(self):
+        """
+        Test invalid inputs for the `calc_rh` function.
+
+        It tests whether the function raises the expected exception for the given inputs.
+
+        """
+        # Test if the function raises a TypeError for non-float input
+        with pytest.raises(TypeError):
+            calc_rh('0.5', 1.0)  # type: ignore
+        with pytest.raises(TypeError):
+            calc_rh(0.5, '1.0')  # type: ignore
+
+        # Test if the function raises a ValueError for out-of-range input
+        with pytest.raises(ValueError):
+            calc_rh(2.1, 1.0)
+        with pytest.raises(ValueError):
+            calc_rh(0.5, 0.1)
+        with pytest.raises(ValueError):
+            calc_rh(0.5, 2.1)
+
+    def test_calc_rh_division_by_zero(self):
+        """
+        Test the `calc_rh` function for division by zero.
+
+        It tests whether the function returns 0 when the denominator of the calculation is zero.
+
+        """
+        assert calc_rh(0.0, 1.0) == 0
+        with pytest.raises(ValueError):
+            calc_rh(0.0, 0.0)
+
+
+class TestMinSlope:
+    """
+    Class contain tests the min_slope function.
+    """
+    def test_min_slope_filling_greater_than_0_3(self):
+        assert round(min_slope(0.4, 1.0), 2) == 0.71
+
+    def test_min_slope_filling_less_than_or_equal_to_0_3(self):
+        assert round(min_slope(0.2, 1.0), 2) == 1.27
+
+    def test_min_slope_invalid_filling_value(self):
+        with pytest.raises(ValueError):
+            min_slope(2.0, 1.0)
+
+    def test_min_slope_invalid_diameter_value(self):
+        with pytest.raises(ValueError):
+            min_slope(0.5, 0.1)
+
+    def test_min_slope_invalid_filling_and_diameter_value(self):
+        with pytest.raises(ValueError):
+            min_slope(2.0, 0.1)
+
+    def test_min_slope_valid_filling_and_diameter_values(self):
+        assert round(min_slope(0.5, 1.0), 2) == 0.61
+
+    def test_min_slope_with_theta_and_g_values(self):
+        assert round(min_slope(0.5, 1.0, theta=1.2, g=9.81), 2) == 0.49
+
+    @pytest.mark.parametrize("filling, diameter, expected", [
+        (0.1, 1.0, 2.4071926038740865),
+        (0.2, 1.0, 1.2679613277936983),
+        (0.3, 1.0, 0.8944912435239528),
+        (0.4, 1.0, 0.7137552908970533),
+        (0.5, 1.0, 0.6116207951070336),
+        (0.6, 1.0, 0.550723538128701),
+        (0.7, 1.0, 0.5161624734677323),
+        (0.8, 1.0, 0.5026580734069008),
+        (0.9, 1.0, 0.5130415616498445),
+        (0.1, 2.0, 2.348685545379234),
+        (0.2, 2.0, 1.2035963019370433),
+        (0.3, 2.0, 0.8231546097405654),
+        (0.4, 2.0, 0.6339806638968492),
+        (0.5, 2.0, 0.5214128935469622),
+        (0.6, 2.0, 0.4472456217619764),
+        (0.7, 2.0, 0.39512094038839024),
+        (0.8, 2.0, 0.3568776454485266),
+        (0.9, 2.0, 0.3280021275695537),
+    ])
+    def test_min_slope(self, filling, diameter, expected):
+        assert min_slope(filling, diameter) == pytest.approx(expected, rel=1e-3)
+
+    def test_min_slope_with_custom_theta(self):
+        assert min_slope(0.4, 1.0, theta=2.0) == pytest.approx(0.9516737211960711, rel=1e-3)
+
+    def test_min_slope_with_custom_g(self):
+        assert min_slope(0.4, 1.0, g=10.0) == pytest.approx(0.7001939403700093, rel=1e-3)
+
+    def test_min_slope_with_invalid_input(self):
+        with pytest.raises(TypeError):
+            min_slope("0.4", 1.0)  # type: ignore
