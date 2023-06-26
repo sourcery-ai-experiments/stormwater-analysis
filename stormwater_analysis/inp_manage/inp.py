b4f6e45bd1271d01ea5491a815cd36faff6ecfa7
BuczynskiRafal <buczynski.rafal2@gmail.com>
2023-06-26 06:58:06 +0200

    WIP

--- stormwater_analysis/inp_manage/inp.py
+++ stormwater_analysis/inp_manage/inp.py
@@ -3,7 +3,7 @@ from typing import Dict, List
 import swmmio
 from swmmio.utils.functions import trace_from_node
 
-from stormwater_analysis.data.data import ConduitsData, NodeData, SubcatchmentData
+from stormwater_analysis.data.data import ConduitsData, NodesData, SubcatchmentsData
 from stormwater_analysis.pipes.round import min_slope
 
 
@@ -14,7 +14,7 @@ class SwmmModel:
     Attributes:
         model (swmmio.Model): The original SWMM model.
         conduits_data (ConduitsData): The processed conduits data after feature engineering.
-        nodes_data (NodeData): The processed nodes data after feature engineering.
+        nodes_data (NodesData): The processed nodes data after feature engineering.
         subcatchments_data (SubcatchmentsData): The processed subcatchments data after feature engineering.
     """
 
@@ -22,8 +22,8 @@ class SwmmModel:
         self,
         model: swmmio.Model,
         conduits_data: ConduitsData,
-        nodes_data: NodeData,
-        subcatchments_data: SubcatchmentData,
+        nodes_data: NodesData,
+        subcatchments_data: SubcatchmentsData,
     ) -> None:
         """
         Initializes a SwmmModel object with the given SWMM model and processed data.
@@ -31,7 +31,7 @@ class SwmmModel:
         Args:
             model (swmmio.Model): The original SWMM model.
             conduits_data (ConduitsData): The processed conduits data after feature engineering.
-            nodes_data (NodeData): The processed nodes data after feature engineering.
+            nodes_data (NodesData): The processed nodes data after feature engineering.
             subcatchments_data (SubcatchmentsData): The processed subcatchments data after feature engineering.
         """
         self.model = model
@@ -51,6 +51,36 @@ class SwmmModel:
         """
         outfalls = self.model.inp.outfalls.index
         return {outfall: trace_from_node(self.conduits_data.conduits, outfall) for outfall in outfalls}
+
+    def find_overflowing_pipes(self) -> Dict[str, List[str]]:
+        """
+        Finds all overflowing pipes in the SWMM model.
+        
+        1. Określić na których kanałach sieć jest przepełniona.
+            * Zwróć liste kanałów, które są w koleumnie conduits['ValMaxFill'] == 0 
+        2. Zwrócić lista kanałów, które są przepełnione.
+            * zwrócić trasę kanałów, które są przepełnione na jednej sieci od ostatiego do pierwszego czyli odpłwy - dopływ
+        """
+
+    
+    def place_pipes_to_apply_change(self) -> None:
+        """
+        Places pipes to apply a change in the SWMM model.
+
+        1. Na podstawie listy przepełnionych kanałów określić
+           gdzie należy zastosować rekomendowaną zmianę techniczną.
+        2. Zwrócić liste kanałów lub studzienek na których zastosować zmianę. 
+        """
+    
+    def generate_technical_recommendation(self) -> None:
+        """
+        Generates a technical recommendation in the SWMM model.
+        
+        1. Użyj wytrenowanego ANN do generowania rekomendacji technicznej.
+        2. Przygotuj format danych w jakich bęziesz przechowywał rekomendację. 
+        3. Zapisz dane do pliku.
+        4. Zwrócić użytkownikowi rekomendację. 
+        """
 
     def optimize_conduit_slope(self) -> None:
         # Currently, this function is not needed.
