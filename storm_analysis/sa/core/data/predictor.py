602dcf6605b7faa98d45296199127043c33d6688
Rafał <buczynski.rafal2@gmail.com>
2024-05-05 13:47:01 +0200

    Integrate NN model for catchment classification (#17)
    Introduce to applications catchment classification functionality. It implements a neural network model that processes input data to predict classifications, which are then inserted into the results table for further analysis. The integration ensures a smooth data flow from input to output within the application framework, forming part of the feature engineering used to generate recommendations.

--- storm_analysis/sa/core/data/predictor.py
+++ storm_analysis/sa/core/data/predictor.py
@@ -4,7 +4,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 from tensorflow.keras.models import load_model  # noqa
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(current_directory, "catchment_classification_model", "catchemnt_classifier")
+model_path = os.path.join(current_directory, "catchemnt_classifier", "model.keras")
 
 try:
     classifier = load_model(model_path)
